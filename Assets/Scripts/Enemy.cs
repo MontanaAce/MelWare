3b9ccf4cbb955ebf6f08a0a7c7760f0fc2949a38
Nate Nicholson <nchlsnnate@gmail.com>
2020-11-05 11:00:45 -0600

    Adding Austin's stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public GameObject deathEffect;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            ObjectPool.Spawn(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+    public GameObject deathEffect;
+    public Text healthText;
+
+    private void Start()
+    {
+        healthText.text = health + "/" + maxHealth;
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        UpdateText();
+    }
+    private void UpdateText()
+    {
+        healthText.text = health + "/" + maxHealth;
+    }
+
+}
