a38d95585589819e507757d01d8451a82539ca64
xvalshx <45045940+xvalshx@users.noreply.github.com>
2020-11-05 11:22:11 -0600

    player spawn

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] GameObject mainCamera;
+    public Vector3 playerSpawnPoint;
+    public GameObject player;
+    public float playerHealth;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if(FindObjectOfType<PlayerHandler>() == null)
-        {
-            Object.Instantiate(player, this.transform);
-            Object.Instantiate(mainCamera, this.transform);
-        }
+        playerSpawnPoint = this.transform.position;
+        playerHealth = player.GetComponent<PlayerHealth>().health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerHealth = player.GetComponent<PlayerHealth>().health;
+        if (playerHealth <= 0)
+        {
+            RespawnPlayer();
+        }
     }
+    public void RespawnPlayer()
+    {
+        player.transform.position = playerSpawnPoint;
+    }
+
+
 }
