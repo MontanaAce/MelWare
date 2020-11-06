d1880dbcd5f9abf91bed1fe09f18b8fc7808e188
xvalshx <45045940+xvalshx@users.noreply.github.com>
2020-11-06 11:46:13 -0600

    added health pickup and spawn point logic

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    PlayerHealth playerHealth;
+    public int healthAdd = 5;
+
+
+    void Awake()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(playerHealth.health < playerHealth.maxHealth)
+        {
+            Destroy(gameObject);
+            playerHealth.health += healthAdd;
+            playerHealth.UpdateText();
+        }
+    }
+}
--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -6,28 +6,26 @@ public class PlayerSpawn : MonoBehaviour
 {
     public Vector3 playerSpawnPoint;
     public GameObject player;
-    public float playerHealth;
+    public PlayerHealth playerHealth;
 
 
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
+        player = FindObjectOfType<PlayerController>().gameObject;
+        playerHealth = FindObjectOfType<PlayerHealth>();
         playerSpawnPoint = this.transform.position;
-        playerHealth = player.GetComponent<PlayerHealth>().health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerHealth = player.GetComponent<PlayerHealth>().health;
-        if (playerHealth <= 0)
-        {
-            RespawnPlayer();
-        }
+
     }
     public void RespawnPlayer()
     {
+        playerHealth.health = playerHealth.maxHealth;
         player.transform.position = playerSpawnPoint;
     }
 
