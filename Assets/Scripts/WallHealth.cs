64a4dfd37f46619169324014cf209054a2cd77ea
Nate Nicholson <nchlsnnate@gmail.com>
2020-11-11 10:36:26 -0600

    Added "cracked wall" concept

---
+++ Assets/Scripts/WallHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHealth : MonoBehaviour
+{
+    public float health;
+    private void Update()
+    {
+        if( health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
