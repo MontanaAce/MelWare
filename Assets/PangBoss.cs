8ec4058eac59dd971b30d59da894b7938f0ae719
xvalshx <45045940+xvalshx@users.noreply.github.com>
2020-12-01 16:28:51 -0600

    added boss animations and some animation logic

---
+++ Assets/PangBoss.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PangBoss : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    private float timeBtwDamage = 1.5f;
+
+    public Animator anim;
+    public Slider healthBar;
+
+
+    private void Update()
+    {
+        if(timeBtwDamage > 0)
+        {
+            timeBtwDamage -= Time.deltaTime;
+        }
+
+        healthBar.value = health;
+    }
+}
